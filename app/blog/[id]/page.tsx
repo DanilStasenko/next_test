a86cad2587bdd06eb694d92542889f2abae86c23
DanilStasenko <stasenkodanil@gmail.com>
2023-07-24 12:26:26 +0400

    add strapi

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,11 +1,17 @@
 import { Metadata } from "next";
 
 async function getData(id: string) {
-	const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+	const response = await fetch(`http://localhost:1337/api/blogs/${id}`, {
+		headers: {
+			authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`
+		},
 		next: {
 			revalidate: 60, //будет раз в минуту делать запрос на сервер для обновления данных, новая фича в next.js
 		}
 	});
+
+	if (!response.ok) throw new Error("Unable to fetch posts")
+
 	return response.json()
 }
 
@@ -16,19 +22,19 @@ type Props = {
 }
 
 export async function generateMetadata({params: {id}} : Props): Promise<Metadata> {
-	const post = await getData(id);
+	const post = await getData(id).then((data) => data.data);
 	return {
-		title: post.title
+		title: post.attributes.title
 	}
 }
 
 const Post = async ({params: {id}} : Props) => {
-  const post = await getData(id);
-
+  const post = await getData(id).then((data) => data.data);
 	return (  
 		<>
-			<h1>{post.title}</h1>
-			<p>{post.body}</p>
+			<h1>{post.attributes.title}</h1>
+			<p>{post.attributes.article}</p>
+			<p>{post.attributes.date}</p>
 		</>
 	);
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,8 +5,11 @@ export const metadata: Metadata = {
   title: 'Blog',
 }
 
-async function getData() {
-	const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+async function getDataFromStrapi() {
+	const response = await fetch('http://localhost:1337/api/blogs/', {
+		headers: {
+			authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`
+		},
 		next: {
 			revalidate: 60, //будет раз в минуту делать запрос на сервер для обновления данных, новая фича в next.js
 		}
@@ -14,25 +17,30 @@ async function getData() {
 
 	if (!response.ok) throw new Error("Unable to fetch posts")
 
-	return response.json()
+	return response.json();
 }
 
 interface Post {
-  userId: number;
   id: number;
-  title: string;
-  body: string;
+  attributes: {
+    title: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    article: string;
+    date: string;
+  };
 }
 
 const Blog = async () => {
-	const posts = await getData();;
+	const posts = await getDataFromStrapi().then((data) => data.data);
 	return (  
 		<>
 			<h1>Blog page</h1>
 			<ul style={{padding: "20px 0"}}>
 				{posts.map((post: Post) => (
 					<li key={post.id}>
-						<Link href={`/blog/${post.id}`}>{post.title}</Link>
+						<Link href={`/blog/${post.id}`}>{post.attributes.title}</Link>
 					</li>
 				))}
 			</ul>
